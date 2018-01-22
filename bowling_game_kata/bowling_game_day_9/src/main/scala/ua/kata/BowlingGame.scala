e201cf778ab9224b7ee447cc7a1aeeac9fed81fa
Alex-Diez <alex.dukhno@icloud.com>
2018-01-22 23:09:35 +0200

    bowling game kata ninth day

---
+++ bowling_game_kata/bowling_game_day_9/src/main/scala/ua/kata/BowlingGame.scala
@@ -0,0 +1,35 @@
+package ua.kata
+
+import akka.actor.Actor
+
+import scala.annotation.tailrec
+
+class BowlingGame extends Actor {
+  private var rolls: List[Int] = List.empty
+
+  override def receive: Receive = {
+    case Roll(pin) => rolls = pin :: rolls
+    case ComputeScore() =>
+      val player = sender()
+      player ! ScoreComputed(score(0, 1, rolls.reverse))
+  }
+
+
+  @tailrec
+  private def score(currentScore: Int, frameIndex: Int, rolls: List[Int]): Int = {
+    def isSpare: Boolean = rolls.take(2).sum == 10
+
+    def spareBonus: Int = rolls.slice(2, 3).sum
+
+    def isStrike: Boolean = rolls.head == 10
+
+    def strikeBonus: Int = rolls.slice(1, 3).sum
+
+    (frameIndex, rolls) match {
+      case (11, _) => currentScore
+      case (_, _ :: rest) if isStrike => score(10 + strikeBonus + currentScore, frameIndex + 1, rest)
+      case (_, _ :: _ :: rest) if isSpare => score(10 + spareBonus + currentScore, frameIndex + 1, rest)
+      case (_, _ :: _ :: rest) => score(rolls.take(2).sum + currentScore, frameIndex + 1, rest)
+    }
+  }
+}
